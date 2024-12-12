aceaa823299e4aa1c8e9471c7682bd2dfe94f4eb
Joe Bracey <joebracey1@gmail.com>
2024-12-12 19:49:52 +0000

    Login and signup added to the app.jsx page

--- geekhq-app/src/App.jsx
+++ geekhq-app/src/App.jsx
@@ -6,39 +6,51 @@ import FAQS from './pages/faq'
 import Profile from './pages/profile'
 import Events from './pages/events'
 import Error from './pages/error'
+import Login from './pages/login'
+import Signup from './pages/signup'
+import { UserContext, UserProvider } from './contexts/UserContext'
 
 
 function App() {
   return (
     <>
-
-    <Router>
-      <div>
-        <nav>
-          <ul className="sidebar">
-            <li className="nav-link">
-              <Link to='/'>Home</Link>
-            </li>
-            <li className="nav-link">
-              <Link to='/faq'>FAQ</Link>
-            </li>
-            <li className="nav-link">
-              <Link to='/profile'>Profile</Link>
-            </li>
-            <li className="nav-link">
-              <Link to='/events'>Events</Link>
-            </li>
-          </ul>
-        </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/faq" element={<FAQS />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </div>
-    </Router>
+    <UserProvider>
+      <Router>
+        <div>
+          <nav>
+            <ul className="sidebar">
+              <li className="nav-link">
+                <Link to='/'>Home</Link>
+              </li>
+              <li className="nav-link">
+                <Link to='/faq'>FAQ</Link>
+              </li>
+              <li className="nav-link">
+                <Link to='/profile'>Profile</Link>
+              </li>
+              <li className="nav-link">
+                <Link to='/events'>Events</Link>
+              </li>
+              <li className="nav-link">
+                <Link to='/signup'>Signup</Link>
+              </li>
+              <li className="nav-link">
+                <Link to='/login'>Login</Link>
+              </li>
+            </ul>
+          </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/faq" element={<FAQS />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+      </Router>
+    </UserProvider>
     </>
   );
 }
